a8680783d7b635c7148a331a19bc7577bb1dd3b5
Nijraj Gelani <Nijraj Gelani@DESKTOP-Q4R65QN>
2017-07-27 21:56:59 +0530

    Add project files.

---
+++ ProgressTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UpdateProgress;
+
+namespace ProgressTest {
+    class Program {
+        static void Main(string[] args) {
+            UpdatableLine line = new UpdatableLine();
+            Console.WriteLine("First line");
+            line.Display("Hello, World");
+            Console.WriteLine("Third line");
+            line.Update("Updated!");
+            Console.WriteLine("Fourth Line");
+            int i = 1;
+            while (i < 100) {
+                System.Threading.Thread.Sleep(300);
+                line.Update(i++ + "%");
+            }
+        }
+    }
+}
---
+++ UpdateProgress/UpdatableLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateProgress {
+    public class UpdatableLine {
+        public int Top;
+        
+        public void Display(string message) {
+            Top = Console.CursorTop;
+            Console.WriteLine(message);
+        }
+
+        public void Update(string message) {
+            int backToTop = Console.CursorTop,
+                backToLeft = Console.CursorLeft ;
+            Console.CursorTop = Top;
+            Console.CursorLeft = 0;
+            for (int i = 0; i < Console.WindowWidth; Console.Write(" "), i++) ;
+            Console.CursorLeft = 0;
+            Console.CursorTop = Top;
+            Console.Write(message);
+            Console.CursorTop = backToTop;
+            Console.CursorLeft = backToLeft;
+        }
+    }
+}
